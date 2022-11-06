2a5eca56a25cdcd7b7ab9f08c1f985798f59c501
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-11-06 11:26:10 -0300

    feat: :truck: improve import to npm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,88 @@
 /* eslint-disable linebreak-style */
-import Box from './components/box';
+/* eslint-disable import/export */
 
-export default {
-  Box
-};
+// components
+
+export * from './components/box';
+
+export * from './components/button';
+
+export * from './components/content';
+
+export * from './components/input';
+export * from './components/input/GTInput';
+
+export * from './components/loader';
+
+export * from './components/login';
+
+export * from './components/modal';
+export * from './components/modal/gt';
+
+export * from './components/motion';
+
+export * from './components/navbar';
+export * from './components/navbar/indexMobile';
+export * from './components/navbar/gtNavbar';
+export * from './components/navbar/gtMobile';
+
+export * from './components/popup';
+
+export * from './components/space';
+
+export * from './components/switch';
+export * from './components/switch/gt';
+
+export * from './components/symbol';
+
+export * from './components/text';
+
+// context
+
+export * from './context/gt';
+
+export * from './context/pageState';
+
+// gt
+
+export * from './gt/global/style';
+
+export * from './gt/gtContainer';
+
+export * from './gt/gtDesign';
+
+// hooks
+
+export * from './hooks/helpers/useEaseClose';
+
+export * from './hooks/helpers/useIsFirstRender';
+
+export * from './hooks/helpers/useIsMobile';
+
+export * from './hooks/helpers/useOnClickOutside';
+
+export * from './hooks/pageState/useInputValues';
+
+export * from './hooks/validation/useInputValidation';
+
+export * from './hooks/validation/useValidateEmail';
+
+export * from './hooks/validation/useValidatePassword';
+
+export * from './hooks/validation/useValidateState';
+
+export * from './hooks/validation/useValidateText';
+
+// utils
+
+export * from './utils/transitions';
+
+export * from './utils/flex';
+
+export * from './utils/hovers';
+
+export * from './utils/randomNumber';
+
+export * from './utils/shadows';
+
+export * from './utils/transitions';
