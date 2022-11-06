5da5c297d10c09d43ec8670e87e6505fc936d640
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-11-06 11:36:19 -0300

    fix: :ambulance: uggly typo

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
 /* eslint-disable linebreak-style */
-import Box from './src/components/box';
+/* eslint-disable import/export */
 
-export const teste = 'teste';
+// components
 
-export default Box;
+export * from './src/components/box';
 
-// export const Navbar = GTNavbar;
+export * from './src/components/button';
+
+export * from './src/components/content';
+
+export * from './src/components/input';
+export * from './src/components/input/GTInput';
+
+export * from './src/components/loader';
+
+export * from './src/components/login';
+
+export * from './src/components/modal';
+export * from './src/components/modal/gt';
+
+export * from './src/components/motion';
+
+export * from './src/components/navbar';
+export * from './src/components/navbar/indexMobile';
+export * from './src/components/navbar/gtNavbar';
+export * from './src/components/navbar/gtMobile';
+
+export * from './src/components/popup';
+
+export * from './src/components/space';
+
+export * from './src/components/switch';
+export * from './src/components/switch/gt';
+
+export * from './src/components/symbol';
+
+export * from './src/components/text';
+
+// context
+
+export * from './src/context/gt';
+
+export * from './src/context/pageState';
+
+// gt
+
+export * from './src/gt/global/style';
+
+export * from './src/gt/gtContainer';
+
+export * from './src/gt/gtDesign';
+
+// hooks
+
+export * from './src/hooks/helpers/useEaseClose';
+
+export * from './src/hooks/helpers/useIsFirstRender';
+
+export * from './src/hooks/helpers/useIsMobile';
+
+export * from './src/hooks/helpers/useOnClickOutside';
+
+export * from './src/hooks/pageState/useInputValues';
+
+export * from './src/hooks/validation/useInputValidation';
+
+export * from './src/hooks/validation/useValidateEmail';
+
+export * from './src/hooks/validation/useValidatePassword';
+
+export * from './src/hooks/validation/useValidateState';
+
+export * from './src/hooks/validation/useValidateText';
+
+// utils
+
+export * from './src/utils/transitions';
+
+export * from './src/utils/flex';
+
+export * from './src/utils/hovers';
+
+export * from './src/utils/randomNumber';
+
+export * from './src/utils/shadows';
+
+export * from './src/utils/transitions';
